60988619eb6f3c818cc42723cc321b33a55588f3
Rajan Panneer Selvam <rajan.india@yahoo.co.in>
2026-06-09 08:57:00 -0400

    Tested the book reader in IPhone

--- mobile/ios/BookForgeReader/AppDelegate.swift
+++ mobile/ios/BookForgeReader/AppDelegate.swift
@@ -62,7 +62,13 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: ".expo/.virtual-metro-entry")
+    if let metroURL = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: ".expo/.virtual-metro-entry") {
+      return metroURL
+    }
+
+    // Default debug builds to this Mac's LAN Metro endpoint so launching from
+    // the iPhone home screen does not fail with a nil script URL.
+    return URL(string: "http://192.168.1.152:8083/.expo/.virtual-metro-entry.bundle?platform=ios&dev=true&hot=false")
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
